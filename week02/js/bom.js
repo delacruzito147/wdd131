fe7525d2106d99aa0cbbbb5ba7e167241515b3b1
delacruzito147 <delacruzito147@gmail.com>
2026-03-12 14:38:36 -0500

    commit

--- week02/js/bom.js
+++ week02/js/bom.js
@@ -2,13 +2,26 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener('click', function() {
 
-li.textContent = input.value;
+  if (input.value.trim() !== '') {
 
-deleteButton.textContent = '❌';
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
 
-li.append(deleteButton);
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
 
-list.append(li);
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+    input.focus();
+  }
+
+});
